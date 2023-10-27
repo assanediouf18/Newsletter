5883fefbdde4423d0887d5aa15c76366e9c6d594
adiouf <h8799r@gmail.com>
2023-10-27 20:17:30 +0200

    Now binding to a random available port

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use std::net::TcpListener;
 use newsletter::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("Listener running at : 127.0.0.1:{port}");
+    run(listener)?.await
 }
