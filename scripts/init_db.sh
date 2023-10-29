48fd6cdac2c47848f7f0268ce9773a0df16601d7
adiouf <h8799r@gmail.com>
2023-10-29 13:28:25 +0100

    Added first migration

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -8,14 +8,16 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-# Launch postgres using docker
-docker run \
-  -e POSTGRES_USER=${DB_USER} \
-  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-  -e POSTGRES_DB=${DB_NAME} \
-  -p "${DB_PORT}":5432 \
-  -d postgres \
-  postgres -N 1000
+if [[ -z "${SKIP_DOCKER}" ]]; then
+  # Launch postgres using docker
+  docker run \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    -d postgres \
+    postgres -N 1000
+fi
 
 export PGPASSWORD="${POSTGRES_PASSWORD}"
 until psql.exe -h localhost -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
@@ -23,8 +25,9 @@ until psql.exe -h localhost -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q
 	sleep 1
 done
 
->&2 echo "Postgress is up and running on port ${DB_PORT}"
+>&2 echo "Postgres is up and running on port ${DB_PORT}"
 
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
-sqlx.exe database create --database-url "$DATABASE_URL"
+sqlx.exe database create --database-url $DATABASE_URL
+sqlx.exe migrate run --database-url $DATABASE_URL
