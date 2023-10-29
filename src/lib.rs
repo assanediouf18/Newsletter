66482b6807a3a3809d2d1a2e646dd515db6dc22d
adiouf <h8799r@gmail.com>
2023-10-29 12:55:35 +0100

    Script to create postgres db (docker)

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,26 @@ use actix_web::{App, HttpResponse, HttpServer, Responder, web};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscriptions))
     })
         .listen(listener)?
         .run();
     Ok(server)
 }
 
+async fn subscriptions(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
